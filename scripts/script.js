284771d35260a245cbe509c59f9f5bafb833abb9
Diego González Liarte <diegogliarte@gmail.com>
2022-12-21 17:22:27 +0100

    generate menu dinamically

---
+++ scripts/script.js
@@ -0,0 +1,55 @@
+function readTextFile(file)
+{
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                alert(allText);
+            }
+        }
+    }
+    rawFile.send(null);
+}
+
+fetch('../products/menu.txt')
+    .then(response => response.text())
+    .then(text => {
+        const lines = text.split("\r\n")
+        const products = document.getElementById("products")
+        let category = null
+        for (let line of lines) {
+            let comma_splitted = line.split(";")
+            if (comma_splitted.length === 1) {
+                category = create_div(["category"])
+                products.appendChild(category)
+                let category_title = create_div(["category-title"], comma_splitted[0])
+                category.appendChild(category_title)
+            } else if (comma_splitted.length >= 1) {
+                let name = comma_splitted[0]
+                let price = comma_splitted[1]
+                console.log(["name"] + comma_splitted.slice(2))
+                let div_name = create_div(["name"].concat(comma_splitted.slice(2)), name)
+                let div_price = create_div(["price"], price)
+                let div_item = create_div(["item"])
+                div_item.append(div_name, div_price)
+                category.appendChild(div_item)
+            }
+        }
+
+    })
+
+function create_div(classes, content = null) {
+    let div = document.createElement("div")
+    for (let class_ of classes) {
+        div.classList.add(class_)
+    }
+    if (content != null) {
+        div.textContent = content
+    }
+    return div
+}
